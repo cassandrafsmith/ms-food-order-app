859beee29296c9e78269c90aa6208201cb0772e0
Cassandra Smith <65835968+cassandrafsmith@users.noreply.github.com>
2022-07-02 22:08:01 -0500

    add cart logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 
 const HeaderCartButton = (props) =>{
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+    const { items } =cartCtx;
+    const numberOfCartItems = items.reduce((total, current) => {
+        return total + current.amount
+    }, 0);
+    
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(()=> {
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [items]);
+
     return(
-        <button className={classes.button} onClick={props.onClick} >
+        <button className={btnClasses} onClick={props.onClick} >
             <span className={classes.Icon}>
                 <CartIcon />
             </span>
@@ -13,7 +39,7 @@ const HeaderCartButton = (props) =>{
                 Your Cart
             </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
